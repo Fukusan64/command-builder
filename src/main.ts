261a9bddeab01d56d4a85ef1382f4521948a1fa1
Fukusan64 <ikishikawa64@gmail.com>
2026-03-21 00:56:59 +0900

    一旦単体で動くようにする

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,54 @@
-import { hello } from './hello.ts';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
-hello();
+const execAsync = promisify(exec);
+
+const createShell = (shell: string | ((commands: string) => Promise<string>) = 'sh') => {
+  let executor: (commands: string) => Promise<string>;
+
+  if (typeof shell === 'string') {
+    executor = async (command: string) => {
+      const result = await execAsync(command, { shell });
+      if (result.stderr) {
+        throw new Error(result.stderr);
+      }
+      return result.stdout;
+    }
+  } else {
+    executor = shell;
+  }
+
+  const f = (commands: string[] = []): any => new Proxy(() => commands, {
+    apply(target, _thisArg, argumentsList) {
+      if (argumentsList.length === 0) {
+        const commands = target();
+        let chainedCommands = commands[0];
+        for (let i = 1; i < commands.length; i++) {
+          const isBeforeRedirect = commands[i - 1].includes('>') || commands[i - 1].includes('<');
+          const isRedirect = commands[i].includes('>') || commands[i].includes('<');
+          if (isBeforeRedirect || isRedirect) {
+            chainedCommands += ` ${commands[i]}`;
+          } else {
+            chainedCommands += ` | ${commands[i]}`;
+          }
+        }
+        return executor(chainedCommands);
+      } else {
+        const commands = [...target()];
+        commands[commands.length - 1] += ` '${argumentsList[0]}'`;
+        return f(commands);
+      }
+    },
+  
+    get(target, prop) {
+      if (typeof prop === 'symbol') throw new Error('Symbol is not allowed');
+      const commands = [...target(), prop];
+      return f(commands);
+    }
+  });
+
+  return f();
+}
+
+const $ = createShell('bash');
+console.log(await $.echo('console.log("Hello, world!")').node());

6491754721ff5409cfed1d8a715d68b6c89e14e4
Fukusan64 <ikishikawa64@gmail.com>
2026-03-22 22:46:58 +0900

    サブコマンドを扱えるようにしたり、複数引数を渡しやすくしたり

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { promisify } from 'node:util';
 
 const execAsync = promisify(exec);
 
+const escapeQuotation = (arg: string) => arg.replace(/'/g, "\\'");
+
 type CommandBuilder = {
   // 実行（引数なしで呼ぶ）
   (): Promise<string>;
-  // 引数追加（1引数で呼ぶ）
-  (argument: string): CommandBuilder;
+  // 引数追加
+  (...args: string[]): CommandBuilder;
+  (args: string[]): CommandBuilder;
   // `proxy.get()` で `shell.ls` みたいに任意のプロパティをコマンドとして扱う
   [command: string]: CommandBuilder;
 };
@@ -29,8 +32,8 @@ export const createShell = (
     executor = shell;
   }
 
-  const f = (commands: string[] = []): CommandBuilder => {
-    return new Proxy(() => commands, {
+  const f = (commands: string[] = []): CommandBuilder =>
+    new Proxy(() => commands, {
       apply(target, _thisArg, argumentsList) {
         if (argumentsList.length === 0) {
           const commands = target();
@@ -49,7 +52,14 @@ export const createShell = (
           return executor(chainedCommands);
         } else {
           const commands = [...target()];
-          commands[commands.length - 1] += ` '${argumentsList[0]}'`;
+          let args: string[];
+          if (Array.isArray(argumentsList[0])) {
+            args = argumentsList[0];
+          } else {
+            args = argumentsList;
+          }
+          commands[commands.length - 1] +=
+            ` ${args.map((arg) => `'${escapeQuotation(arg)}'`).join(' ')}`;
           return f(commands);
         }
       },
@@ -60,7 +70,24 @@ export const createShell = (
         return f(commands);
       },
     }) as unknown as CommandBuilder;
-  };
 
   return f();
 };
+
+export function w(str: string): string[];
+export function w(
+  template: { raw: readonly string[] | ArrayLike<string> },
+  ...substitutions: string[]
+): string[];
+export function w(
+  arg1: string | { raw: readonly string[] | ArrayLike<string> },
+  ...rest: string[]
+): string[] {
+  let str: string;
+  if (typeof arg1 === 'string') {
+    str = arg1;
+  } else {
+    str = String.raw({ raw: arg1.raw }, ...rest);
+  }
+  return str.split(' ');
+}
